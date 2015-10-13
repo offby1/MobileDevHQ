7b031d8396c5e5b5d23ad064a585f61a1d38a01f
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-10-13 12:06:46 -0700

    Make it more object-y

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,13 +1,24 @@
 import sys
 
-def read_board(inf):
-    board = []
-    for line in inf:
-        line = line.rstrip()
-        cells = line.split(',')
-        board.append(cells)
-    return board
+class Board:
+    def __init__(self, cell_array):
+        self.cells = cell_array
+
+    @classmethod
+    def from_file(cls, inf):
+        cell_array = []
+        for line in inf:
+            line = line.rstrip()
+            cells = line.split(',')
+            cell_array.append(cells)
+        return cls(cell_array)
+
+    def solve(self):
+        return self
 
 if __name__ == "__main__":
     import pprint
-    pprint.pprint(read_board(sys.stdin))
+    b = Board.from_file(sys.stdin)
+    pprint.pprint(b)
+    solution = b.solve()
+    pprint.pprint(solution)
